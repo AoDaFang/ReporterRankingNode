1002b1b87ffda4b7a449ae4ea216aa813641fb7f
aodafang <1426490132@qq.com>
2020-05-07 15:47:58 +0800

    修改入口文件

--- rank.js
+++ rank.js
@@ -4,5 +4,6 @@ const egg = require('egg');
 // const workers = Number(process.argv[2] || require('os').cpus().length); // 获取cup数量
 egg.startCluster({
   workers: 1,
-  baseDir: __dirname
+  baseDir: __dirname,
+  port: 8082,
 });
